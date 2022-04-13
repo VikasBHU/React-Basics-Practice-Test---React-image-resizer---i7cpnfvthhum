302e70ee81b3ad08c270ebfdaa1421650e634845
Vikas Pandey <96241726+VikasBHU@users.noreply.github.com>
2022-04-13 15:23:43 +0530

    React Basics Practice Test - React image resizer

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <div id="main"></div>;
+  let [height, setheight] = useState(320);
+  let [width, setwidth] = useState(320);
+  const handleheightchange = (e) => {
+    setheight(e.target.value);
+  };
+  const handlewidthchange = (e) => {
+    setwidth(e.target.value);
+  };
+  return (
+    <div id="main">
+      <img
+        src="./src/image.jpeg"
+        id="resizable-img"
+        height={height}
+        width={width}
+      />
+      <br />{" "}
+      <input
+        type="range"
+        id="height-slider"
+        min={100}
+        max={800}
+        onChange={handleheightchange}
+      />
+      <br />
+      <input
+        type="range"
+        id="width-slider"
+        min={100}
+        max={800}
+        onChange={handlewidthchange}
+      />
+    </div>
+  );
 };
 
 export default App;
